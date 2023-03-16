5b8e4f04cbd03cec727fd5c4820d71b486c69488
Maria Luiza <Marialuizacamilo92@gmail.com>
2023-03-15 22:35:09 -0300

    meu comite é bonito

---
+++ app/Models/Sorvete.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sovete extends Model
+{
+    use HasFactory;
+
+    // Indica o nome da tabela do banco de dados
+    protected $table = "sorvetes";
+
+    // Indica os campos que podem ser preenchidos pelo usuário
+    protected $fillable = [
+        'nome',
+        'sabor',
+        'valor',
+
+
+
+    ];
+}
